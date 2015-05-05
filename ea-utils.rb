716a82f0c49664ecb57ef90e459409fb5cbb2045
madMilo <camilo@nouserinterface.com>
2015-05-05 16:21:01 -0500

    Working ea-utils

--- ea-utils.rb
+++ ea-utils.rb
@@ -1,20 +1,23 @@
 require 'formula'
 #adpated from https://github.com/galaxyproject/homebrew-toolshed/blob/master/jjohnson_fastqmcf.rb
-class Fastqmcf < Formula
-  version "1.0"
+class  EaUtils < Formula
+  version "1.1.2-484"
   # Recipe auto-generate from repository https://toolshed.g2.bx.psu.edu/jjohnson/fastq_mcf
-  # Tool Shed Readme:
   option "without-architecture", "Build without allowing architecture information (to force source install when binaries are available)."
                
   url "http://ea-utils.googlecode.com/files/ea-utils.1.1.2-484.tar.gz"
   sha256 "92a2cc90b39c1868cb2cbbbb40fe7251d8327518066ec941648b9dd5ac04e785"
   
   depends_on "google-sparsehash"
+  depends_on "gsl"
                    
   def install
-    system "sed -i.bak 's/varcall$//' Makefile" 
-    system "PREFIX=#{prefix} make install" 
-    bin.install "sam-stats"
-    # Tool Shed set environment variable that is picked implicitly.
+    inreplace "sam-stats.cpp" do |s|
+      s.gsub! /<samtools\/sam.h>/, "\"samtools\/sam.h\""
+    end
+    ENV.append "CFLAGS", "-I#{Formula["google-sparsehash"].include}" 
+    system "rm -Rf google"
+    system "PREFIX=#{prefix} make install"
+    bin.install %w(fastq-mcf fastq-multx fastq-join fastq-stats fastq-clipper sam-stats varcall)
   end
 end
