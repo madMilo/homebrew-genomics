2a4035d3dc0d8ca8ab26e5e46301288b739269cf
madMilo <camilo@nouserinterface.com>
2015-05-05 15:14:10 -0500

    try3

--- fastqmcf.rb
+++ fastqmcf.rb
@@ -7,8 +7,10 @@ class Fastqmcf < Formula
   option "without-architecture", "Build without allowing architecture information (to force source install when binaries are available)."
                
   url "http://ea-utils.googlecode.com/files/ea-utils.1.1.2-484.tar.gz"
-  sha1 ""
-                     
+  sha256 "92a2cc90b39c1868cb2cbbbb40fe7251d8327518066ec941648b9dd5ac04e785"
+  
+  depends_on "google-sparsehash"
+                   
   def install
     system "sed -i.bak 's/varcall$//' Makefile" 
     system "PREFIX=#{prefix} make install" 
